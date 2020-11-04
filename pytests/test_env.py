4d2e7fd0e0bc7c0824ce25f2760746379d9f7bc0
adam-trudgeon <trudgeon.adam@gmail.com>
2020-11-03 16:29:58 -0800

    Added fixtures to initiallize the test_client for url tests

--- pytests/test_env.py
+++ pytests/test_env.py
@@ -2,27 +2,28 @@ import pytest
 from postproject import app
 from postproject import routes
 
+@pytest.fixture
+def client():
+    client = app.test_client() 
+    return client    
+
 #pytest for app URLs
-def test_home_route():
-    client = app.test_client()
+def test_home_route(client):
     url = '/'
     response = client.get(url)
     assert response.status_code == 200
 
-def test_login_route():
-    client = app.test_client()
+def test_login_route(client):
     url = '/login'
     response = client.get(url)
     assert response.status_code == 200
 
-def test_register_route():
-    client = app.test_client()
+def test_register_route(client):
     url = '/register'
     response = client.get(url)
     assert response.status_code == 200
 
-def test_postits_route():
-    client = app.test_client()
+def test_postits_route(client):
     url = '/post_its'
     response = client.get(url)
     assert response.status_code == 200
